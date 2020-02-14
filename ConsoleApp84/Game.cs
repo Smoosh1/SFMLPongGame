3bebcce2bf40eb2f56be248a39e3132849bb852c
Smoosh1 <55028372+Smoosh1@users.noreply.github.com>
2020-02-15 01:32:06 +0200

    Code refactor + smth new

--- ConsoleApp84/Game.cs
+++ ConsoleApp84/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -7,12 +7,22 @@ namespace Program
 {
     internal class Game
     {
-        private const uint Height = 900;
-        private const uint Width = 1800;
-        private const int FPSLimit = 120;
-        private uint BallSize = 50;
+        private const ushort Height = 900;
+        private const ushort Width = 1800;
+        private const byte FPSLimit = 120;
+
+        private byte score;
+        private string ScoreText = "0";
+        private ushort BallSize = 50;
+
+        private static float BatHeight = 300f;
+        private static float BatWidth = 50f;
 
         private float timeStep = 1f / FPSLimit;
+
+
+        static float BatPosY = Height - BatHeight * 2;
+        static float BatTwoPosX = Width - BatWidth;
 
         CircleShape Ball;
         RenderWindow window;
@@ -29,6 +39,21 @@ namespace Program
         private Vector2f Velocity;
         private Vector2f SpeedOfBats;
 
+      /*  private void Score()
+        {
+            if (CollisonLogic() == 1)
+                score++;
+            Font font;
+            Text text(ScoreText, font); 
+        }*/
+
+        private void LoseMechanic()
+        {
+            if(Ball.Position.X== 0 || Ball.Position.X == Width)
+                IsEndGame = true;    
+        }
+
+
         public void Init()
         {
             window = new RenderWindow(new VideoMode(Width, Height), "Ball game");
@@ -36,14 +61,12 @@ namespace Program
 
             Ball = InitShape(BallSize, new Vector2f(600f, 300f), Color.Red);
 
-            LeftBat = InitPlatform(new Vector2f(50f, 300f), new Vector2f(0f, 300f), Color.Yellow);
-            RightBat = InitPlatform(new Vector2f(50f, 300f), new Vector2f(1750f, 300f), Color.Magenta);
+            LeftBat = InitPlatform(new Vector2f(BatWidth, BatHeight), new Vector2f(0f, BatPosY), Color.Yellow);
+            RightBat = InitPlatform(new Vector2f(BatWidth, BatHeight), new Vector2f(BatTwoPosX, BatPosY), Color.Magenta);
 
             SpeedOfBats = new Vector2f(0f, 5f);
             Velocity = new Vector2f(500f, 400f);
         }
-
-
 
         internal void Run()
         {
@@ -51,10 +74,11 @@ namespace Program
 
             while (IsGameRun())
             {
+                LoseMechanic();
+
                 GetInput();
 
                 TryMove();
-
 
                 LogicOfCirle();
 
@@ -62,29 +86,52 @@ namespace Program
 
                 CollisonLogic();
 
-                Draw();
 
+                Draw();
             }
         }
 
-
-        private void Draw()
+        private void GetInput()
         {
-            window.Clear();
+            Key_W_Is_Pressed = false;
+            Key_S_Is_Pressed = false;
+            Key_ArrowUp_Is_Pressed = false;
+            Key_ArrowDown_Is_Pressed = false;
 
-            Ball.FillColor = Color.Red;
-            Ball.Draw(window, RenderStates.Default);
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                IsEndGame = true;
 
-            LeftBat.Draw(window, RenderStates.Default);
-            RightBat.Draw(window, RenderStates.Default);
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+                Key_W_Is_Pressed = true;
 
-            window.Display();
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+                Key_S_Is_Pressed = true;
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+                Key_ArrowUp_Is_Pressed = true;
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+                Key_ArrowDown_Is_Pressed = true;
+        }
+
+        private void TryMove()
+        {
+            if (LeftBat.Position.Y == 0)
+                Key_W_Is_Pressed = false;
+
+            if (LeftBat.Position.Y == Height - BatHeight)
+                Key_S_Is_Pressed = false;
+
+            if (RightBat.Position.Y == 0)
+                Key_ArrowUp_Is_Pressed = false;
+
+            if (RightBat.Position.Y == Height - BatHeight)
+                Key_ArrowDown_Is_Pressed = false;
         }
 
         private void LogicOfCirle()
         {
             Ball.Position += Velocity * timeStep;
-
 
             if (Ball.Position.Y + 2 * BallSize > Height && Velocity.Y > 0)
                 Velocity.Y *= -1;
@@ -101,8 +148,6 @@ namespace Program
 
         private void LogicOfPlatforms()
         {
-
-
             if (Key_W_Is_Pressed == true)
                 LeftBat.Position -= SpeedOfBats;
 
@@ -117,70 +162,32 @@ namespace Program
                 RightBat.Position += SpeedOfBats;
         }
 
-        private void GetInput()
+        private int CollisonLogic()
         {
-            Key_W_Is_Pressed = false;
-            Key_S_Is_Pressed = false;
-            Key_ArrowUp_Is_Pressed = false;
-            Key_ArrowDown_Is_Pressed = false;
+            if (LeftBat.Position.X + BatWidth > Ball.Position.X && LeftBat.Position.Y + BatHeight == Ball.Position.Y)
+                Velocity *= -1;
 
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
-                IsEndGame = true;
+            if (RightBat.Position.X < Ball.Position.X + 2 * BallSize && RightBat.Position.Y == Ball.Position.Y)
+                Velocity *= -1;
 
-            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
-                Key_W_Is_Pressed = true;
+            return 1;   //это недоделано, в будущем для текста 
+        }
+        private void Draw()
+        {
+            window.Clear();
 
+            Ball.FillColor = Color.Red;
+            Ball.Draw(window, RenderStates.Default);
 
-            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
-                Key_S_Is_Pressed = true;
+            LeftBat.Draw(window, RenderStates.Default);
+            RightBat.Draw(window, RenderStates.Default);
 
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
-                Key_ArrowUp_Is_Pressed = true;
-
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
-                Key_ArrowDown_Is_Pressed = true;
-
-
+            window.Display();
         }
 
         private bool IsGameRun()
         {
             return window.IsOpen && !IsEndGame;
-        }
-
-        private void TryMove()
-        {
-            if (LeftBat.Position.Y == 0)
-                Key_W_Is_Pressed = false;
-
-            if (LeftBat.Position.Y == 600)
-                Key_S_Is_Pressed = false;
-
-            if (RightBat.Position.Y == 0)
-                Key_ArrowUp_Is_Pressed = false;
-
-            if (RightBat.Position.Y == 600)
-                Key_ArrowDown_Is_Pressed = false;
-
-
-
-
-
-
-        }
-
-        private void CollisonLogic()
-        {
-
-
-            if (LeftBat.Position.X + 50f > Ball.Position.X)
-                Velocity *= -1;
-
-            if (RightBat.Position.X < Ball.Position.X + 2 * BallSize)
-                Velocity *= -1;
-
-
-
         }
 
         private CircleShape InitShape(float size, Vector2f position, Color color)
@@ -191,7 +198,6 @@ namespace Program
             return Ball;
         }
 
-
         private RectangleShape InitPlatform(Vector2f size, Vector2f position, Color color)
         {
             RectangleShape Platfom = new RectangleShape(size);
@@ -199,6 +205,6 @@ namespace Program
             Platfom.Position = position;
             return Platfom;
         }
-ы
+
     }
 }
